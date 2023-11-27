f895c67037ef86a4daf95a09701d8a7a5cdd51ab
amandeepkaile <119895472+amandeepkaile@users.noreply.github.com>
2023-11-27 15:38:52 +1100

    Add files via upload
    Signed-off-by: amandeepkaile <119895472+amandeepkaile@users.noreply.github.com>

---
+++ MyUtils.xcframework/ios-arm64_x86_64-simulator/MyUtils.framework/Headers/MyUtils.h
@@ -0,0 +1,18 @@
+//
+//  MyUtils.h
+//  MyUtils
+//
+//  Created by Amandeep Kaile on 27/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyUtils.
+FOUNDATION_EXPORT double MyUtilsVersionNumber;
+
+//! Project version string for MyUtils.
+FOUNDATION_EXPORT const unsigned char MyUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyUtils/PublicHeader.h>
+
+
